566945cf5a4712725870067903ce4f6bb8d56452
Kevin Karahali <kevin.karahali@hotmail.com>
2021-08-02 16:47:09 +0200

    cambio en index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World! Kevin')
+    res.send('Hello World! Desde Docker!!!')
 })
 
 app.listen(port, () => {
